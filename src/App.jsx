446f54291ca7aa54de4a610f08d6bdbee459cd61
Manav Chawla <manav.chawla@sifars.com>
2023-12-14 11:55:48 +0530

    fixed file issue

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
 import RequiresAuth from "./RequiresAuth";
 import HomePage from "./pages/Homepage";
-import LoginForm from "./Login";
+import LoginForm from "./pages/Login";
 
 const App = () => {
   return (
